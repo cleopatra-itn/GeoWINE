4163547a4c141dfd3a411e8b35d445aa5f4a2ee3
Endri Kacupaj <endrikacupaj@gmail.com>
2021-02-06 12:30:17 +0100

    - add sample routes and data to api

--- api/api.py
+++ api/api.py
@@ -1,8 +1,25 @@
-import time
-from flask import Flask
+import json
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+sample_results = [
+    {
+        'id': 'Q172251',
+        'label': 'Yabrud',
+        'link': 'https://www.wikidata.org/wiki/Q172251',
+        'coordinates': [33.967222, 36.657222],
+        'news_articles': [],
+        'events': []
+    }
+]
+
+@app.route('/select', methods=['POST'])
+def query_selected_image():
+    data = request.get_json()
+    id = data['id']
+    return 'Response data send succefully!' # sample_results
+
+@app.route('/upload', methods=['POST'])
+def query_upload_image():
+    return sample_results
