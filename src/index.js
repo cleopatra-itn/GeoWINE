61ece867613a1bd8c7481e9ff3aee6b23bfab544
Endri Kacupaj <endrikacupaj@gmail.com>
2021-02-06 12:31:54 +0100

    - update import paths

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "bootswatch/dist/sketchy/bootstrap.min.css";
+import 'index.css';
+import App from 'App';
+import reportWebVitals from 'reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
