2829a3d0f6f926eb4ba82b2b6d50d8517beb35db
Endri Kacupaj <endrikacupaj@gmail.com>
2021-02-06 12:33:33 +0100

    - add news articles base class

---
+++ src/components/NewsArticles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NewsArticles extends React.Component {
+    render () {
+        return (
+            <p>News Articles</p>
+        );
+    }
+}
+
+export default NewsArticles;
