f5f29080ad8104da49e7f3e9fa0f7c321730de82
Endri Kacupaj <endrikacupaj@gmail.com>
2021-02-06 12:33:59 +0100

    - add upload image base class

---
+++ src/components/UploadImage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class UploadImage extends React.Component {
+    render () {
+        return (
+            <div>
+                <p>Upload image!</p>
+            </div>
+        );
+    }
+}
+
+export default UploadImage;
