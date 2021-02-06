398ccb151a3a6bc406ed01104572f5771559ec9d
Endri Kacupaj <endrikacupaj@gmail.com>
2021-02-06 12:33:19 +0100

    - add map base class with leaflet integration

---
+++ src/components/Map.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Leaflet from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+Leaflet.Icon.Default.imagePath =
+'../node_modules/leaflet'
+
+delete Leaflet.Icon.Default.prototype._getIconUrl;
+
+Leaflet.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+class Map extends React.Component {
+    render () {
+        return (
+            <div>
+                <MapContainer className="jumbotron" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{height: "500px", width: "100%"}}>
+                    <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
+        );
+    }
+}
+
+export default Map;
