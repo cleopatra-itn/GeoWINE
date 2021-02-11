4d3bd92c9bdff35ce7a38ea054a9e9290d0e5e2c
Endri Kacupaj <endrikacupaj@gmail.com>
2021-02-11 11:10:07 +0100

    - update api for new sample data

--- api/api.py
+++ api/api.py
@@ -3,23 +3,36 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-sample_results = [
-    {
-        'id': 'Q172251',
-        'label': 'Yabrud',
-        'link': 'https://www.wikidata.org/wiki/Q172251',
-        'coordinates': [33.967222, 36.657222],
-        'news_articles': [],
-        'events': []
-    }
-]
+SAMPLE_RESULTS_ENTITIES = {
+    'Q2981': json.load(open('sample_data/entities/Q2981.json')),
+    'Q82878': json.load(open('sample_data/entities/Q82878.json'))
+}
 
-@app.route('/select', methods=['POST'])
-def query_selected_image():
+SAMPLE_RESULTS_NEWS = {
+    'Q2981': json.load(open('sample_data/news/Q2981.json')),
+    'Q82878': json.load(open('sample_data/news/Q82878.json'))
+}
+SAMPLE_RESULTS_EVENTS = {
+    'Q2981': json.load(open('sample_data/events/Q2981.json')),
+    'Q82878': json.load(open('sample_data/events/Q82878.json'))
+}
+
+
+@app.route('/select_image_entities', methods=['POST'])
+def selected_image_entities():
     data = request.get_json()
     id = data['id']
-    return 'Response data send succefully!' # sample_results
+    return SAMPLE_RESULTS_ENTITIES[id]
+
+@app.route('/select_image_news_events', methods=['POST'])
+def selected_image_news_events():
+    data = request.get_json()
+    id = data['id']
+    return {
+        'news': SAMPLE_RESULTS_NEWS[id],
+        'events': SAMPLE_RESULTS_EVENTS[id]
+    }
 
 @app.route('/upload', methods=['POST'])
 def query_upload_image():
-    return sample_results
+    return {}
