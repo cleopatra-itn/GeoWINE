a7bda0ab735409f356db71260b2456a71c3009f6
Endri Kacupaj <endrikacupaj@gmail.com>
2021-02-15 16:08:50 +0100

    - create news card content

--- src/components/NewsArticles.js
+++ src/components/NewsArticles.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 class NewsArticles extends React.Component {
     constructor(props) {
@@ -18,7 +19,23 @@ class NewsArticles extends React.Component {
 
     render () {
         return (
-            <p>News Articles: {this.state.data}</p>
+            <Card className={'border-light mb-3'} style={{height: "510px", overflowY: "auto", padding: '1.25rem'}}>
+                {this.state.data.map((article, _) => (
+                    <Card className="border-light mb-3">
+                        <Card.Body>
+                            <Card.Title>{article.title}</Card.Title>
+                            <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'nowrap', justifyContent: 'space-between'}}>
+                                <Card.Subtitle className="mb-2 text-muted">Source: {article.source}</Card.Subtitle>
+                                <Card.Subtitle className="mb-2 text-muted">{article.date}</Card.Subtitle>
+                            </div>
+                            <Card.Text className={'cut-text'}>
+                                {article.body}
+                            </Card.Text>
+                            <Card.Link href={article.url} target='_blank'>Read more</Card.Link>
+                        </Card.Body>
+                    </Card>
+                ))}
+            </Card>
         );
     }
 }
