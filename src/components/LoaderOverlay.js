c2dc7b1b2a517f230cc3b4e1209557ed0b4e945c
Endri Kacupaj <endrikacupaj@gmail.com>
2021-03-02 00:35:40 +0100

    - add LoderOverlay

---
+++ src/components/LoaderOverlay.js
@@ -0,0 +1,12 @@
+function LoaderOverlay() {
+    return (
+        <div id="overlay">
+            <div class="lds-circle">
+                <div class="lds-circle-div"></div>
+                <div class="lds-circle-text">Loading...</div>
+            </div>
+        </div>
+    );
+  }
+
+export default LoaderOverlay;
