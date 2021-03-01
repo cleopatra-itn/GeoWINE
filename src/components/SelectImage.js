12a356ae81f40bbef20031709ee7d13876a42922
Endri Kacupaj <endrikacupaj@gmail.com>
2021-03-02 00:32:35 +0100

    - update SelectImage component (add types, radius, etc.)

--- src/components/SelectImage.js
+++ src/components/SelectImage.js
@@ -4,30 +4,37 @@ import { Row, Col } from 'react-bootstrap';
 import Select from 'react-select';
 import ImageGallery from 'react-image-gallery';
 import axios from 'axios';
+import $ from 'jquery';
 import "react-image-gallery/styles/css/image-gallery.css";
 
 const IMAGES = [
     {
       original: require('images/resized/Q2981.jpg').default,
       thumbnail: require('images/thumbnail/Q2981.jpg').default,
-      id: 'Q2981'
-    },
-    {
-      original: require('images/resized/Q82878.jpg').default,
-      thumbnail: require('images/thumbnail/Q82878.jpg').default,
-      id: 'Q82878'
+      id: 'notreparis.jpg' // 'Q2981'
     }
 ];
 
 const optionsTypes = [
-    { value: 'Q570116', label: 'Tourist Attraction' },
-    { value: 'Q33506', label: 'Museum' },
+    { value: 'Q570116', label: 'Tourist Attraction' }, // 2870
+    { value: 'Q33506', label: 'Museum' }, // 40031
+    { value: 'Q839954', label: 'Archaeological Site' }, // 39211
+    { value: 'Q618123', label: 'Geographical Feature' }, // 6604
+    { value: 'Q2319498', label: 'Landmark' }, // 627
+    { value: 'Q43229', label: 'Organization' }, // 70283
+    { value: 'Q327333', label: 'Government Agency' }, // 12729
+    { value: 'Q1802963', label: 'Mansion' }, // 1323
+    { value: 'Q162875', label: 'Mausoleum' }, // 2061
+    { value: 'Q2221906', label: 'Geographic Location' }, // 8300
+    { value: 'Q2065736', label: 'Cultural Property' }, // 66003
+    { value: 'Q41176', label: 'Building' } //
 ];
 
 const optionsRadius = [
-    { value: '5', label: '5KM' },
-    { value: '10', label: '10KM' },
-    { value: '20', label: '20KM' },
+    { value: '1', label: 'Street Level (1KM)' },
+    { value: '25', label: 'City Level (25KM)' },
+    { value: '200', label: 'Region Level (200KM)' },
+    { value: '750', label: 'Country Level (750KM)' }
 ];
 
 class SelectImage extends React.Component {
@@ -43,7 +50,7 @@ class SelectImage extends React.Component {
 			activeIndex: 0,
 			errorMessage: '',
             selectedTypeOption: { value: 'Q570116', label: 'Tourist Attraction' },
-            selectedRadiusOption: { value: '5000', label: '5KM' }
+            selectedRadiusOption: { value: '25', label: 'City Level (25KM)' }
         };
 
         this.handleClick = this.handleClick.bind(this);
@@ -53,17 +60,22 @@ class SelectImage extends React.Component {
 
     handleClick () {
         // POST data and get results
+        console.log('Sending')
         var data = {
             id: IMAGES[this.state.activeIndex]['id'],
             type: this.state.selectedTypeOption.value,
             radius: this.state.selectedRadiusOption.value,
         };
-        axios.post('/select_image_entities', data) // submit to api and get results
+        $("#overlay").fadeIn(300);
+        axios.post('/api/select_image_entities', data) // submit to api and get results
             .then(response => {
+                console.log(response.data);
+                $("#overlay").fadeOut(300);
                 this.props.inputImageCallback(response.data); // pass response data to parent
 			})
             .catch(error => {
             	this.setState({ errorMessage: error.message });
+                $("#overlay").fadeOut(300);
             	console.error('There was an error while requesting results for the selected image!', error);
             });
     }
