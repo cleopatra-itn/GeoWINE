f0f6f9f95401021bd8ad6da180ec2b1db1b8d5a0
Endri Kacupaj <endrikacupaj@gmail.com>
2021-03-02 00:31:18 +0100

    - add geowine on api.py and add new sample data

--- api/api.py
+++ api/api.py
@@ -1,30 +1,43 @@
+import os
 import json
+from pathlib import Path
+from geo_wine import GeoWINE
 from flask import Flask, request
+
+ROOT_PATH = Path(os.path.dirname(__file__))
+
+geowine = GeoWINE()
 
 app = Flask(__name__)
 
-SAMPLE_RESULTS_ENTITIES = {
-    'Q2981': json.load(open('sample_data/entities/Q2981.json')),
-    'Q82878': json.load(open('sample_data/entities/Q82878.json'))
+SAMPLE_IMAGES = {
+    'notreparis.jpg': {
+        'image': f'{ROOT_PATH}/sample_images/notreparis.jpg',
+        'true_coords': [48.852966, 2.349902]
+    }
 }
 
-SAMPLE_RESULTS_NEWS = json.load(open('sample_data/news/news.json'))
-SAMPLE_RESULTS_EVENTS = json.load(open('sample_data/events/events.json'))
-
-@app.route('/select_image_entities', methods=['POST'])
+@app.route('/api/select_image_entities', methods=['POST'])
 def selected_image_entities():
     data = request.get_json()
-    id = data['id']
-    return SAMPLE_RESULTS_ENTITIES[id]
 
-@app.route('/select_image_news_events', methods=['POST'])
+    id = data['id']
+    img_path = SAMPLE_IMAGES[id]['image']
+    true_coords = SAMPLE_IMAGES[id]['true_coords']
+
+    radius = data['radius']
+    entity_type = data['type']
+
+    return geowine.retrieve_entities_with_image_path(path=img_path, radius=radius, entity_type=entity_type, true_coords=true_coords)
+
+@app.route('/api/select_image_news_events', methods=['POST'])
 def selected_image_news_events():
     data = request.get_json()
-    id = data['id']
+    # id = data['id']
+    # label = data['label']
     return {
-        'id': id,
-        'news': SAMPLE_RESULTS_NEWS[id],
-        'events': SAMPLE_RESULTS_EVENTS[id]
+        'id': data['id'],
+        **geowine.retrieve_news_events(data)
     }
 
 @app.route('/upload', methods=['POST'])
