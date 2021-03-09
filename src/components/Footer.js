e43d2f3e81a99f039de1221b99e584591b461dd1
endrikacupaj <endrikacupaj@gmail.com>
2021-03-09 10:50:34 +0100

    - update github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ class Footer extends React.Component {
                     <div style={{marginBottom: '1rem'}}>
                         <InfoModal /> |
                         <ReferenceModal /> |
-                        <Nav.Link style={{display: 'inline', padding: '0.5rem'}} href="https://github.com/cleopatra-itn/geolocation-demo.git" target="_blank">GitHub</Nav.Link>
+                        <Nav.Link style={{display: 'inline', padding: '0.5rem'}} href="https://github.com/cleopatra-itn/GeoWINE" target="_blank">GitHub</Nav.Link>
                     </div>
                     <span>&copy; Copyright 2021: GeoWINE - v{0.1}</span><br/>
                     <span className='App-footer-references'>Built with <a href="https://getbootstrap.com/" target="_blanck">Bootstrap</a>. Theme from <a href="https://bootswatch.com/" target="_blanck">Bootswatch</a>.</span>
